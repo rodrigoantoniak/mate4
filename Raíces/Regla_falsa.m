ffdd8fd9447308beb16280ec0ff391a47ccef88b
Rodrigo Lionel Antoniak <rodrigolionelantoniak@gmail.com>
2021-06-09 18:27:55 -0300

    Recursividad incluida
    Ya no se necesita probar con valores intermedios para conocer las raíces, el programa hará la recursividad necesaria

--- Raíces/Regla_falsa.m
+++ Raíces/Regla_falsa.m
@@ -1,61 +1,112 @@
-% Metodo de Regla Falsa.
 warning("off")
 pkg load symbolic
-syms f(x) aux
-f(x)=input('Ingrese funcion a analizar:');
-xl=input('Ingrese limite inferior del intervalo:');
-xu=input('Ingrese limite superior del intervalo:');
-tol=input('Ingrese la tolerancia deseada:');
-xr=0;
-n=0;
-MEP=0;
-fprintf(' n       xl          xr          xu         MEP\n');
-
-do
-    aux=xu-((xu-xl)*f(xu)/(f(xu)-f(xl)));
-    xr=double(aux);
-    aux=f(xr);
-    if(aux>=0)
-        MEP=double(aux);
-    else
-        MEP=double(aux)*(-1);
-    end;
-    fprintf('%02d   ', n);
-    if(xl>=0)
-        fprintf('+');
-    end;
-    fprintf('%f   ', xl);
-    if(xr>=0)
-        fprintf('+');
-    end;
-    fprintf('%f   ', xr);
-    if(xu>=0)
-        fprintf('+');
-    end;
-    fprintf('%f   %f\n', xu, MEP);
-    if((f(xl)*f(xr)<0) && (f(xu)*f(xr)>0))
-        xu=xr;
-    elseif((f(xl)*f(xr)>0) && (f(xu)*f(xr)<0))
-        xl=xr;
-    else
-        break;
-    end;
-    n=n+1;
-until(MEP<=tol);
-
-if((f(xl)==0) || (f(xu)==0))
-    fprintf('Raiz encontrada: ');
-    if(f(xl)==0)
-        fprintf('%f\n', xl);
-    else
+syms f(x) y(x)
+function biseccion(f,xl,xu,tol)
+    syms aux
+    xr=0;
+    n=0;
+    MEP=0;
+    fprintf(' n       xl          xr          xu         MEP\n');
+    do
+        aux=xu-((xu-xl)*f(xu)/(f(xu)-f(xl)));
+        xr=double(aux);
+        aux=f(xr);
+        if(aux>=0)
+            MEP=double(aux);
+        else
+            MEP=double(aux)*(-1);
+        end;
+        fprintf('%02d   ', n);
+        if(xl>=0)
+            fprintf('+');
+        end;
+        fprintf('%f   ', xl);
+        if(xr>=0)
+            fprintf('+');
+        end;
+        fprintf('%f   ', xr);
+        if(xu>=0)
+            fprintf('+');
+        end;
+        fprintf('%f   %f\n', xu, MEP);
+        if((f(xl)*f(xr)<0) && (f(xu)*f(xr)>0))
+            xu=xr;
+        elseif((f(xl)*f(xr)>0) && (f(xu)*f(xr)<0))
+            xl=xr;
+        else
+            break;
+        end;
+        n=n+1;
+    until(MEP<=tol);
+    if(MEP<=tol)
+        fprintf('Raiz encontrada con una tolerancia maxima de %f:\n\t', tol);
+        if(xr>=0)
+            fprintf('+');
+        end;
+        fprintf('%f\n', xr);
+    elseif((f(xl)*f(xr)<0) && (f(xu)*f(xr)<0))
+        fprintf('Mas de una raiz encontrada. Se intentara nuevamente con:\n');
+        fprintf('>> ');
+        if(xl>=0)
+            fprintf('+');
+        end;
+        fprintf('%f y ', xl);
+        if(xr>=0)
+            fprintf('+');
+        end;
+        fprintf('%f\n>> ', xr);
+        if(xr>=0)
+            fprintf('+');
+        end;
+        fprintf('%f y ', xr);
+        if(xu>=0)
+            fprintf('+');
+        end;
         fprintf('%f\n', xu);
+        fprintf('\nIntento con ');
+        if(xl>=0)
+            fprintf('+');
+        end;
+        fprintf('%f y ', xl);
+        if(xr>=0)
+            fprintf('+');
+        end;
+        fprintf('%f\n', xr);
+        biseccion(f,xl,xr,tol);
+        fprintf('\nIntento con ');
+        if(xr>=0)
+            fprintf('+');
+        end;
+        fprintf('%f y ', xr);
+        if(xu>=0)
+            fprintf('+');
+        end;
+        fprintf('%f\n', xu);
+        biseccion(f,xr,xu,tol);
+    else
+        fprintf('Raiz no encontrable con estos parametros\n');
+    end;
+end;
+y(x)=input('Ingrese funcion a analizar:');
+xi=input('Ingrese limite inferior del intervalo:');
+xd=input('Ingrese limite superior del intervalo:');
+if((y(xi)==0) || (y(xd)==0))
+    fprintf('Raiz encontrada: ');
+    if(f(xi)==0)
+        if(xi>=0)
+            fprintf('+');
+        end;
+        fprintf('%f\n', xi);
+    else
+        if(xd>=0)
+            fprintf('+');
+        end;
+        fprintf('%f\n', xd);
     end
-elseif(MEP<=tol)
-    fprintf('Raiz encontrada con una tolerancia maxima de %f:\n\t%f\n', tol, xr);
-elseif((f(xl)*f(xr)<0) && (f(xu)*f(xr)<0))
-    fprintf('Mas de una raiz encontrada. Intente nuevamente con:\n');
-    fprintf('>> %f y %f\n', xl, xr);
-    fprintf('>> %f y %f\n', xr, xu);
 else
-    fprintf('Raiz no encontrable con estos parametros\n');
+    erro=input('Ingrese la tolerancia deseada:');
+    if(erro<0)
+        erro=erro*(-1);
+    end;
+    biseccion(y,xi,xd,erro);
 end;
